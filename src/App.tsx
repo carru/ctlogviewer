e6873f759699e51b7aa3cdcba104721bd39bec35
carru <carru400@gmail.com>
2022-08-24 20:12:09 -0700

    filter input file by extension

--- src/App.tsx
+++ src/App.tsx
@@ -59,7 +59,7 @@ function App() {
 		<div id="app">
 			{loading && <LoadingMask />}
 			<div id="settings" className='horizontalFlex'>
-				<input type="file" onChange={(e) => loadFile(e.target.files![0])} />
+				<input type="file" accept='.ct.log' onChange={(e) => loadFile(e.target.files![0])} />
 				<button onClick={() => stackTraceRef.current?.collapseExpandAll(false)}>Expand All</button>
 				<button onClick={() => stackTraceRef.current?.collapseExpandAll(true)}>Collapse All</button>
 				<div>
